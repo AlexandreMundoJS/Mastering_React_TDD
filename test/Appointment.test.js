2d09c868b1e9e726cc990b213e23d37e8e6740b8
AlexandreMundoJS <alexangm@gmail.com>
2024-05-28 12:18:30 -0300

    test: pass the first test

--- test/Appointment.test.js
+++ test/Appointment.test.js
@@ -1,5 +1,21 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Appointment } from "../src/Appointment";
+import { act } from "react";
+// import { act } from "react-dom/test-utils";
+// act from react-dom is deprecated
+
 describe("Appointment", () => {
   it("renders the customer first name", () => {
+    const customer = {
+      firstName: "Ashley",
+    };
+    const component = <Appointment customer={customer} />;
+    const container = document.createElement("div");
+    act(() => {
+      ReactDOM.createRoot(container).render(component);
+    });
+    document.body.appendChild(container);
     expect(document.body.textContent).toContain("Ashley");
   });
 });
